4c9a6e76d9660652582d498f393030b58370fcbe
vetal04075 <vetal04075@gmail.com>
2020-10-21 14:06:06 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# AppiumDemo
+demo for react-native 0.60+ with Appium 
+
+
+To check this demo on your device 
+
+Commands to run : 
+
+1- Clone this repo and update the APK path to your path (Absolute Path)
+
+2- npm install 
+
+3- react-native run-android
+
+4- Start the appium server desktop version OR yarn run appium in the terminal 
+
+5- yarn test 
---
+++ __tests__/myTests.test.js
@@ -0,0 +1,21 @@
+import wd from 'wd';
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+const PORT = 4723;
+const config = {
+    platformName: 'Android',
+    deviceName: 'emulator-5554',
+    appiumVersion: '1.18.1',
+    automationName: 'UiAutomator2',
+    app: '/Users/vetal04075/job/react-native/AppiumDemo/android/app/build/outputs/apk/debug/app-debug.apk'
+};
+const driver = wd.promiseChainRemote('localhost', PORT);
+beforeAll(async () => {
+    await driver.init(config);
+    await driver.sleep(3000);
+}) // Sometime for the app to load
+test('appium renders', async () => {
+    expect(await driver.hasElementByAccessibilityId('alertButton')).toBe(true);
+    const element = await driver.elementByAccessibilityId('alertButton')
+    await element.click()
+    expect(await driver.hasElementByAccessibilityId('notHere')).toBe(false);
+});
