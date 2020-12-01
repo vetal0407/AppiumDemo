e040f9e6e08bdcf923e19aab56b9b9e072ba1d07
Vitalii Solovei <vetal04075@gmail.com>
2020-12-01 14:31:31 +0200

    new commit

--- __tests__/myTests.test.js
+++ __tests__/myTests.test.js
@@ -1,17 +1,19 @@
 import wd from 'wd';
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
 const PORT = 4723;
 const config = {
+    automationName: "UiAutomator2",
     platformName: 'Android',
-    deviceName: 'emulator-5554',
+    deviceName: 'emulator',
     appiumVersion: '1.18.1',
-    automationName: 'UiAutomator2',
-    app: '/Users/vetal04075/job/react-native/AppiumDemo/android/app/build/outputs/apk/debug/app-debug.apk'
+    app: '/Users/vagrant/git/android/app/build/outputs/apk/debug/app-debug.apk'
 };
+
+
 const driver = wd.promiseChainRemote('localhost', PORT);
 beforeAll(async () => {
     await driver.init(config);
-    await driver.sleep(3000);
+    await driver.sleep(10000);
 }) // Sometime for the app to load
 test('appium renders', async () => {
     expect(await driver.hasElementByAccessibilityId('alertButton')).toBe(true);
