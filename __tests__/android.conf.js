5ff7d0b983be1c259bef5a5473840e076acd26db
vetal04075 <vetal04075@gmail.com>
2021-02-17 12:22:17 +0200

    migrated to jasmine

--- __tests__/android.conf.js
+++ __tests__/android.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
-  services: ['appium'],
-  port: 4723,
+  //services: ['appium'],
+  port: 10000,
   runner: 'local',
   specs: ['./__tests__/test_cases/**/*.js'],
   capabilities: [
@@ -12,17 +12,32 @@ exports.config = {
       automationName: 'UiAutomator2',
     },
   ],
-  logLevel: 'trace',
+  logLevel: 'debug',
+  deprecationWarnings: true,
   bail: 0,
+  baseUrl: 'http://localhost',
   screenshotPath: './errorShots/',
   waitforTimeout: 50000,
   connectionRetryTimeout: 90000,
   connectionRetryCount: 3,
-  framework: 'mocha',
+  framework: 'jasmine',
   reporters: ['spec'],
-  mochaOpts: {
+  jasmineNodeOpts: {
+    //
+    // Jasmine default timeout
+    defaultTimeoutInterval: 30000,
+    require: ['@babel/register'],
+    //
+    // The Jasmine framework allows interception of each assertion in order to log the state of the application
+    // or website depending on the result. For example, it is pretty handy to take a screenshot every time
+    // an assertion fails.
+    // expectationResultHandler(passed, assertion) {
+    //   // do something
+    // },
+  },
+  /*mochaOpts: {
     ui: 'bdd',
     timeout: 60000,
     require: ['@babel/register'],
-  },
+  },*/
 };
